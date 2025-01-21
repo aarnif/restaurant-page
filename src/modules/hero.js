3f60322d80108bf5d99983f997a38b57e7823a11
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-01-21 21:47:08 +0200

    Make hero section responsive

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -1,11 +1,15 @@
 const Hero = (heroText) => {
   const heroImageContainer = document.createElement("div");
-  heroImageContainer.className = `w-full h-[700px] flex-col-center bg-restaurant bg-center bg-cover shadow-2xl header1`;
+  heroImageContainer.className = `w-full h-[100px] sm:h-[500px] xl:h-[700px] flex flex-col items-center bg-restaurant bg-center bg-cover shadow-2xl header1`;
 
   const heroTextContainer = document.createElement("div");
-  heroTextContainer.className = "max-w-[800px] flex-grow flex-row-center";
-  heroTextContainer.textContent = heroText;
+  heroTextContainer.className =
+    "max-w-[800px] flex-grow hidden sm:flex justify-center items-center";
 
+  const heroTitle = document.createElement("h1");
+  heroTitle.textContent = heroText;
+
+  heroTextContainer.appendChild(heroTitle);
   heroImageContainer.appendChild(heroTextContainer);
 
   return heroImageContainer;
