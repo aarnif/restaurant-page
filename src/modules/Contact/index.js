c3e2dfaf096df1b18456c7512b42075c879bebe6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-01-22 17:31:24 +0200

    Move contact page data into Contact/index.js

--- src/modules/Contact/index.js
+++ src/modules/Contact/index.js
@@ -1,6 +1,30 @@
 import Hero from "../hero";
-import { address, socialMedia, openingHours } from "./data";
 import contactFormSection from "./contactForm";
+
+const address = [
+  "Restaurant Row, Times Square",
+  "374 W 46th Street",
+  "New York, NY 10036",
+  "United States",
+  "(212) FAKE-PHONE-NUMBER",
+  "fake.address@velluto-rosso.com",
+];
+
+const socialMedia = [
+  { platform: "Facebook", link: "https://www.facebook.com" },
+  { platform: "Instagram", link: "https://www.instagram.com" },
+  { platform: "Twitter", link: "https://www.twitter.com" },
+];
+
+const openingHours = [
+  { day: "Sunday", hours: "Closed" },
+  { day: "Monday", hours: "Closed" },
+  { day: "Tuesday", hours: "9:00 AM - 10:00 PM" },
+  { day: "Wednesday", hours: "9:00 AM - 10:00 PM" },
+  { day: "Thursday", hours: "9:00 AM - 10:00 PM" },
+  { day: "Friday", hours: "9:00 AM - 11:00 PM" },
+  { day: "Saturday", hours: "11:00 AM - 12:00 PM" },
+];
 
 const socialMediaIcons = () => {
   const socialMediaIconsList = document.createElement("ul");
